b3c9650736156293a815cd682a49737ae25be33f
BuildTools <unconfigured@null.spigotmc.org>
2020-07-15 23:39:38 -0400

    Advanced Stats component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import RoundButton from "../RoundButton/RoundButton";
+
+const Footer = () => {
+	return (
+		<div className="Footer">
+			<div className="toes">
+				<h2>Boost your links today</h2>
+				<RoundButton value="Get Started" />
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
